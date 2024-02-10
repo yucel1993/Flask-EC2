772343dc4f16f3ca8c3d15a6c91dffc01fe01b6e
Burhan <alpagut1993@gmail.com>
2024-02-10 12:41:41 +0100

    123

--- hello.py
+++ hello.py
@@ -1,14 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 @app.route("/")
-def hello_word():
-    return "<p> Hello Word </p>"
+def hello_world():
+    return "<p>Hello, World!</p>"
 
+@app.route("/products")
+def products():
+    return "<h1>Product Page</h1>"
 
+@app.route("/products/<int:id>")
+def product_detail(id):
+    return f"<h1>Product Detail Page for { id } </h1>"
 
-if __name__ == '__main__':  # We added this to run python hello.py
+@app.route("/example")
+def example():
+    name = 'Rafe'
+    age = 18
+    return render_template('index.html', name=name, age=age)
+
+if __name__ == '__main__':
     # app.run(debug=True)
-    # app.run(debug=True,port=3003)
-    app.run(host='0.0.0.0',port=80)
+    # app.run(debug=True, port=3003)
+    app.run(host='0.0.0.0', port=80)
